bf371e67d6cb4a57e4128cdf281cd031755fb6fd
kazuryu <41988990+Kazuryu0907@users.noreply.github.com>
2024-06-22 14:44:59 +0900

    LearningCircuitに対応

--- scikit_quri/circuit/circuit.py
+++ scikit_quri/circuit/circuit.py
@@ -3,13 +3,14 @@ from typing import Callable
 
 import numpy as np
 from numpy.typing import NDArray
-from quri_parts.circuit import UnboundParametricQuantumCircuit
+from quri_parts.circuit import UnboundParametricQuantumCircuit, ImmutableBoundParametricQuantumCircuit
 
 
 @dataclass
 class LearningCircuit:
     n_qubits: int
     n_parameters: int = field(init=False, default=0)
+    n_thetas: int = field(init=False, default=0)
     circuit: UnboundParametricQuantumCircuit = field(init=False)
     input_functions: dict[int, Callable[[NDArray[np.float_]], float]] = field(
         init=False, default_factory=dict
@@ -28,6 +29,7 @@ class LearningCircuit:
     def add_parametric_RX_gate(self, qubit: int) -> None:
         self.circuit.add_ParametricRX_gate(qubit)
         self.n_parameters += 1
+        self.n_thetas += 1
 
     def add_input_RY_gate(
         self, qubit: int, input_function: Callable[[NDArray[np.float_]], float]
@@ -39,6 +41,7 @@ class LearningCircuit:
     def add_parametric_RY_gate(self, qubit: int) -> None:
         self.circuit.add_ParametricRY_gate(qubit)
         self.n_parameters += 1
+        self.n_thetas += 1
 
     def add_input_RZ_gate(
         self, qubit: int, input_function: Callable[[NDArray[np.float_]], float]
@@ -50,18 +53,38 @@ class LearningCircuit:
     def add_parametric_RZ_gate(self, qubit: int) -> None:
         self.circuit.add_ParametricRZ_gate(qubit)
         self.n_parameters += 1
+        self.n_thetas += 1
+    
+    @property
+    def parameter_count(self) -> int:
+        return self.n_parameters - len(self.input_functions)
     
     def bind_input_and_parameters(
         self, x: NDArray[np.float_], parameters: NDArray[np.float_]
-    ) -> None:
+    ) -> ImmutableBoundParametricQuantumCircuit:
         bound_parameters = []
+        print(f"{self.input_functions=}")
+        parameter_index = 0
         for i in range(self.n_parameters):
             input_function = self.input_functions.get(i)
             if input_function is None:
-                bound_parameters.append(parameters[i])
+                bound_parameters.append(parameters[parameter_index])
+                parameter_index += 1
             else:
                 bound_parameters.append(input_function(x))
-        self.circuit.bind_parameters(bound_parameters)
+        return self.circuit.bind_parameters(bound_parameters)
+    
+    def generate_bound_params(self,x: NDArray[np.float_],parameters: NDArray[np.float_]) -> NDArray[np.float_]:
+        bound_parameters = []
+        parameter_index = 0
+        for i in range(self.n_parameters):
+            input_function = self.input_functions.get(i)
+            if input_function is None:
+                bound_parameters.append(parameters[parameter_index])
+                parameter_index += 1
+            else:
+                bound_parameters.append(input_function(x))
+        return bound_parameters
 
 
 def preprocess_x(x: NDArray[np.float_], i: int) -> float:
@@ -75,6 +98,8 @@ if __name__ == "__main__":
         circuit.add_input_RX_gate(i, lambda x, i=i: np.arcsin(preprocess_x(x, i)))
     circuit.add_parametric_RX_gate(0)
     circuit.add_parametric_RX_gate(1)
-    circuit.bind_input_and_parameters(np.array([0.1, 0.2, 0.3]), np.array([0.4, 0.5]))
+    bind_circuit = circuit.bind_input_and_parameters(np.array([0.1, 0.2, 0.3]), np.array([0.4, 0.5]))
     print(circuit.circuit.gates)
     print(circuit.circuit.param_mapping.in_params)
+    for gate in bind_circuit.gates:
+        print(gate)
--- scikit_quri/qnn/regressor.py
+++ scikit_quri/qnn/regressor.py
@@ -10,16 +10,19 @@ from quri_parts.core.estimator import (
     Estimatable,
     ParametricQuantumEstimator,
 )
-from quri_parts.core.state import ParametricCircuitQuantumState
+from quri_parts.core.state import ParametricCircuitQuantumState,CircuitQuantumState
 from quri_parts.qulacs import QulacsParametricStateT
 from quri_parts.circuit import UnboundParametricQuantumCircuit
-
+from scikit_quri.circuit import LearningCircuit
 from typing import List
+from numpy.random import default_rng
+# ! Will remove
+from quri_parts.qulacs.estimator import create_qulacs_vector_estimator
 
 @dataclass
 class QNNRegressor:
     n_qubits: int
-    ansatz: UnboundParametricQuantumCircuitProtocol
+    ansatz: UnboundParametricQuantumCircuitProtocol | LearningCircuit
     estimator: ParametricQuantumEstimator[QulacsParametricStateT]
     gradient_estimator: ConcurrentParametricQuantumEstimator[QulacsParametricStateT]
     optimizer: Adam
@@ -28,15 +31,29 @@ class QNNRegressor:
     # * batchすればいけそう
     # * learning_parameter_list => update_parameters(theta)
     # * input_parameter_list => 
-    # ? x_trainの埋め込み方がわからん！
+    # * calc_gradどうやってbind分けるか考えといて
+    # ? add_input_RX_gateだと，inputが増えていく
+    # ? 一気にbindするしかなさそう
+    # ? 理想をいうなら，inputとparameterを分けて，parameterに従属な回路を作ってEstimateしたい
     # ? Unboundである必要性がわからん！
-    # ? AdamってcostとかArrayでいいの？
+
+    def __post_init__(self) -> None:
+        pass
 
     def fit(self, x_train: NDArray[np.float_], y_train: NDArray[np.float_],batch_size=32) -> None:
+        if x_train.ndim == 1:
+            x_train = x_train.reshape((-1, 1))
+
         self.x_train = x_train
         self.y_train = y_train
-
-        init_params = np.random.random(self.ansatz.parameter_count)
+        parameter_count = 0
+        if isinstance(self.ansatz, LearningCircuit):
+            parameter_count = self.ansatz.parameter_count
+        else:
+            parameter_count = self.ansatz.parameter_count
+        print(f"{parameter_count=}")
+        np.random.seed(0) 
+        init_params = np.random.random(parameter_count)
         optimizer_state = self.optimizer.get_init_state(init_params)
         x_split = list(self._batch(x_train,batch_size))
         y_spilt = list(self._batch(y_train,batch_size))
@@ -51,12 +68,13 @@ class QNNRegressor:
                     grad_fn_batched
                 )
                 print(f"{optimizer_state.cost=}")
-                # break
+                break
                 if optimizer_state.status == "CONVERGED":
                     break
 
                 if optimizer_state.status == "FAILED":
                     break
+            break
 
     def run(self, x_train: NDArray[np.float_]) -> NDArray[np.float_]:
         # self.ansatz += 1
@@ -65,24 +83,39 @@ class QNNRegressor:
     def cost_fn(self,x_batched:NDArray[np.float_],y_batched:NDArray[np.float_], params: Sequence[float]) -> float:
         y_pred = self._predict_inner(x_batched,params)
         # Case of MSE
-        cost = self._log_loss(y_batched,y_pred)
+        cost = np.mean((y_batched - y_pred) ** 2)
+        # cost = self._log_loss(y_batched,y_pred)
         return cost
 
     def grad_fn(self, x_batched:NDArray[np.float_] ,param_values: Sequence[Sequence[float]]) -> np.ndarray:
-        embed_circuits = self._embed_x_circuit(x_batched)
         grads = []
-        for circuit_state in embed_circuits:
-            grad_estimate = self.gradient_estimator(
-                self.operator,
-                circuit_state,
-                param_values,
-            )
-            grads.append(grad_estimate.values)
+        if isinstance(self.ansatz, LearningCircuit):
+            n_params = self.ansatz.parameter_count
+            n_all_params = self.ansatz.n_parameters
+            for x in x_batched:
+                params = self.ansatz.generate_bound_params(x,param_values)
+                params[0] = 0;params[1] = 0
+                if grads == []:
+                    print(f"{params=}")
+                circuit = ParametricCircuitQuantumState(self.n_qubits, self.ansatz.circuit)
+                estimate = self.gradient_estimator(self.operator,circuit,params)
+                # input用のparamsを取り除く
+                grads.append(estimate.values[n_all_params-n_params:])
+        else:
+            embed_circuits = self._embed_x_circuit(x_batched)
+            for circuit_state in embed_circuits:
+                grad_estimate = self.gradient_estimator(
+                    self.operator,
+                    circuit_state,
+                    param_values,
+                )
+                grads.append(grad_estimate.values)
         grads = np.asarray(grads)
+        print(f"{grads[0]=}")
         grads = np.mean(grads, axis=0)
         grads = np.asarray([g.real for g in grads])
 
-        # print(f"{grads=}")
+
         return grads
     
     def _embed_x_circuit(self,x_scaled: NDArray[np.float_]) -> List[ParametricCircuitQuantumState]:
@@ -130,17 +163,26 @@ class QNNRegressor:
     def _predict_inner(self, x_scaled: NDArray[np.float_], params: Sequence[float]) -> NDArray[np.float_]:
         res = []
         from quri_parts.circuit.utils.circuit_drawer import draw_circuit
-        for x in x_scaled:
-            circuit = UnboundParametricQuantumCircuit(self.n_qubits)
-            for i in range(self.n_qubits):
-                circuit.add_RY_gate(i, np.arcsin(x) * 2)
-                circuit.add_RZ_gate(i, np.arccos(x * x) * 2)
+        if isinstance(self.ansatz, LearningCircuit):
+            for x in x_scaled:
+                params = self.ansatz.generate_bound_params(x,params)
+                parametric_circuit = ParametricCircuitQuantumState(self.n_qubits, self.ansatz.circuit)
+                estimate = self.estimator(self.operator,parametric_circuit,params)
+                res.append(estimate.value.real)
             
-            # bind_circuit = self.ansatz.bind_parameters(params)
-            circuit = circuit.combine(self.ansatz)
-            parametric_circuit = ParametricCircuitQuantumState(self.n_qubits, circuit)
-            estimate = self.estimator(self.operator,parametric_circuit,params)
-            res.append(estimate.value.real)
+
+        else:
+            for x in x_scaled:
+                circuit = UnboundParametricQuantumCircuit(self.n_qubits)
+                for i in range(self.n_qubits):
+                    circuit.add_RY_gate(i, np.arcsin(x) * 2)
+                    circuit.add_RZ_gate(i, np.arccos(x * x) * 2)
+                
+                # bind_circuit = self.ansatz.bind_parameters(params)
+                circuit = circuit.combine(self.ansatz)
+                parametric_circuit = ParametricCircuitQuantumState(self.n_qubits, circuit)
+                estimate = self.estimator(self.operator,parametric_circuit,params)
+                res.append(estimate.value.real)
         
         return res 
             
