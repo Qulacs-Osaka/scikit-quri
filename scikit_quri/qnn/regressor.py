e36129769e4be7c37523142d9819ddb5381b25d5
Toshio Mori <forest1040@gmail.com>
2024-06-27 16:25:34 +0900

    dev

--- scikit_quri/qnn/regressor.py
+++ scikit_quri/qnn/regressor.py
@@ -4,26 +4,37 @@ from dataclasses import dataclass, field
 import numpy as np
 from numpy.typing import NDArray
 from quri_parts.algo.optimizer import Adam
-from quri_parts.circuit.circuit_parametric import UnboundParametricQuantumCircuitProtocol
+from quri_parts.circuit.circuit_parametric import (
+    UnboundParametricQuantumCircuitProtocol,
+)
 from quri_parts.core.estimator import (
     ConcurrentParametricQuantumEstimator,
     Estimatable,
     ParametricQuantumEstimator,
 )
 from quri_parts.algo.optimizer import OptimizerStatus
-from quri_parts.core.state import ParametricCircuitQuantumState,CircuitQuantumState
+from quri_parts.core.state import ParametricCircuitQuantumState, CircuitQuantumState
 from quri_parts.qulacs import QulacsParametricStateT
 from quri_parts.circuit import UnboundParametricQuantumCircuit
 from scikit_quri.circuit import LearningCircuit
 from typing import List
 from numpy.random import default_rng
+
 # ! Will remove
 from quri_parts.qulacs.estimator import create_qulacs_vector_estimator
+
+
+from sklearn.metrics import mean_squared_error
+from scipy.optimize import minimize
+
+iter = 0
+
 
 @dataclass
 class QNNRegressor:
     n_qubits: int
-    ansatz: UnboundParametricQuantumCircuitProtocol | LearningCircuit
+    # ansatz: UnboundParametricQuantumCircuitProtocol | LearningCircuit
+    ansatz: LearningCircuit
     estimator: ParametricQuantumEstimator[QulacsParametricStateT]
     gradient_estimator: ConcurrentParametricQuantumEstimator[QulacsParametricStateT]
     optimizer: Adam
@@ -42,18 +53,20 @@ class QNNRegressor:
     # ! gradで，スライスが恒等である保障はない
     # ! EstimatorをConcurrentにしたほうが，predict_innnerで多次元outputに対応できる
 
-    def minMaxScaler(self,X:NDArray[np.float_],feature_range:tuple[int,int]=(0, 1)):
+    def minMaxScaler(
+        self, X: NDArray[np.float_], feature_range: tuple[int, int] = (0, 1)
+    ):
         """
         Normalize the input data in feature=(min,max).
 
         Parameters:
             X: Input data.
             feature_range: Tuple of (min, max) value of the feature.
-        
+
         Returns:
             X_scaled: Normalized input data.
         """
-        (min,max) = feature_range
+        (min, max) = feature_range
         X_std = (X - X.min(axis=0)) / (X.max(axis=0) - X.min(axis=0))
         X_scaled = X_std * (max - min) + min
         return X_scaled
@@ -62,8 +75,25 @@ class QNNRegressor:
     def __post_init__(self) -> None:
         pass
 
+    def callback(self, xk):
+        global iter
+        print("callback {}: xk={}".format(iter, xk))
+        iter += 1
+
+    def cost_func(
+        self,
+        theta: List[float],
+        x_scaled: NDArray[np.float_],
+        y_scaled: NDArray[np.float_],
+    ) -> float:
+        y_pred = self._predict_inner(x_scaled, theta)
+        cost = mean_squared_error(y_pred, y_scaled)
+        return cost
+
     # !miniBatch処理未実装
-    def fit(self, x_train: NDArray[np.float_], y_train: NDArray[np.float_],batch_size=32) -> None:
+    def fit(
+        self, x_train: NDArray[np.float_], y_train: NDArray[np.float_], batch_size=32
+    ) -> None:
         """
         Fit the model to the training data.
 
@@ -71,27 +101,29 @@ class QNNRegressor:
             x_train: Input data whose shape is (n_samples, n_features).
             y_train: Output data whose shape is (n_samples, n_outputs).
             batch_size: The number of samples in each batch.
-        
+
         """
         if x_train.ndim == 1:
             x_train = x_train.reshape((-1, 1))
-        
+
         if y_train.ndim == 1:
             y_train = y_train.reshape((-1, 1))
-        
+
         if self.do_x_scale:
-            x_scaled = self.minMaxScaler(x_train,feature_range=(-self.x_norm_range,self.x_norm_range))
+            x_scaled = self.minMaxScaler(
+                x_train, feature_range=(-self.x_norm_range, self.x_norm_range)
+            )
         else:
             x_scaled = x_train
-        
+
         if self.do_y_scale:
-            y_scaled = self.minMaxScaler(y_train,feature_range=(-self.y_norm_range,self.y_norm_range))
+            y_scaled = self.minMaxScaler(
+                y_train, feature_range=(-self.y_norm_range, self.y_norm_range)
+            )
         else:
             y_scaled = y_train
-        
 
         self.n_outputs = y_scaled.shape[1]
-
 
         self.x_train = x_scaled
         self.y_train = y_scaled
@@ -110,36 +142,54 @@ class QNNRegressor:
         print(f"{init_params=}")
         optimizer_state = self.optimizer.get_init_state(init_params)
         # batch process
-        x_split = list(self._batch(self.x_train,batch_size))
-        y_spilt = list(self._batch(self.y_train,batch_size))
+        x_split = list(self._batch(self.x_train, batch_size))
+        y_spilt = list(self._batch(self.y_train, batch_size))
 
-        while True:
-            for x_batched,y_batched in zip(x_split,y_spilt):
-                cost_fn_batched = lambda params: self.cost_fn(x_batched,y_batched,params)
-                grad_fn_batched = lambda params: self.grad_fn(x_batched,y_batched,params)
-                optimizer_state = self.optimizer.step(
-                    optimizer_state,
-                    cost_fn_batched, 
-                    grad_fn_batched
-                )
-                print(f"{optimizer_state.cost=}")
-                # break
-            if optimizer_state.status == OptimizerStatus.CONVERGED:
-                break
+        # while True:
+        for x_batched, y_batched in zip(x_split, y_spilt):
+            # cost_fn_batched = lambda params: self.cost_fn(x_batched,y_batched,params)
+            # grad_fn_batched = lambda params: self.grad_fn(x_batched,y_batched,params)
+            # optimizer_state = self.optimizer.step(
+            #     optimizer_state,
+            #     cost_fn_batched,
+            #     grad_fn_batched
+            # )
+            maxiter = 300
+            result = minimize(
+                self.cost_func,
+                init_params,
+                args=(x_batched, y_batched),
+                # method="Nelder-Mead",
+                method="COBYLA",
+                options={"maxiter": maxiter},
+                callback=self.callback,
+            )
+            # optimizer_state.cost = result.fun
+            self.trained_param = result.x
 
-            if optimizer_state.status == OptimizerStatus.FAILED:
-                break
-
-        self.trained_param = optimizer_state.params
-        print(f"{self.trained_param=}")
+            print(f"{optimizer_state.cost=}")
             # break
+            # if optimizer_state.status == OptimizerStatus.CONVERGED:
+            #     break
+
+            # if optimizer_state.status == OptimizerStatus.FAILED:
+            #     break
+
+        # self.trained_param = optimizer_state.params
+        # print(f"{self.trained_param=}")
+        # break
 
     # ! 未実装
     def run(self, x_train: NDArray[np.float_]) -> NDArray[np.float_]:
         # self.ansatz += 1
         pass
 
-    def cost_fn(self,x_batched:NDArray[np.float_],y_batched:NDArray[np.float_], params: Sequence[float]) -> float:
+    def cost_fn(
+        self,
+        x_batched: NDArray[np.float_],
+        y_batched: NDArray[np.float_],
+        params: Sequence[float],
+    ) -> float:
         """
         Calculate the cost function for solver.
 
@@ -151,12 +201,12 @@ class QNNRegressor:
         Returns:
             cost: Cost function value.
         """
-        y_pred = self._predict_inner(x_batched,params)
+        y_pred = self._predict_inner(x_batched, params)
         # Case of MSE
         cost = np.mean((y_batched - y_pred) ** 2)
         return cost
-    
-    def predict(self,x_test:NDArray[np.float_]) -> NDArray[np.float_]:
+
+    def predict(self, x_test: NDArray[np.float_]) -> NDArray[np.float_]:
         """
         Predict outcome for each input data in `x_test`.
 
@@ -171,23 +221,30 @@ class QNNRegressor:
 
         if x_test.ndim == 1:
             x_test = x_test.reshape((-1, 1))
-        
+
         if self.do_x_scale:
-            x_scaled = self.minMaxScaler(x_test,feature_range=(-self.x_norm_range,self.x_norm_range))
-        else:
-            x_scaled = x_test
-        
-        if self.do_y_scale:
-            y_pred = self.minMaxScaler(
-                self._predict_inner(x_scaled,self.trained_param),
-                feature_range=(-self.y_norm_range,self.y_norm_range)
+            x_scaled = self.minMaxScaler(
+                x_test, feature_range=(-self.x_norm_range, self.x_norm_range)
             )
         else:
-            y_pred = self._predict_inner(x_scaled,self.trained_param)
-        
+            x_scaled = x_test
+
+        if self.do_y_scale:
+            y_pred = self.minMaxScaler(
+                self._predict_inner(x_scaled, self.trained_param),
+                feature_range=(-self.y_norm_range, self.y_norm_range),
+            )
+        else:
+            y_pred = self._predict_inner(x_scaled, self.trained_param)
+
         return y_pred
 
-    def grad_fn(self, x_batched:NDArray[np.float_], y_batched:NDArray[np.float_] ,params: Sequence[Sequence[float]]) -> NDArray[np.float_]:
+    def grad_fn(
+        self,
+        x_batched: NDArray[np.float_],
+        y_batched: NDArray[np.float_],
+        params: Sequence[Sequence[float]],
+    ) -> NDArray[np.float_]:
         """
         Calculate the gradient of the cost function for solver.
 
@@ -195,16 +252,16 @@ class QNNRegressor:
             x_batched: Input data whose shape is (batch_size, n_features).
             y_batched: Output data whose shape is (batch_size, n_outputs).
             params: Parameters for the quantum circuit.
-        
+
         Returns:
             grads: Gradient of the cost function.
         """
         if isinstance(self.ansatz, LearningCircuit):
             # ? shape=(batch_size,)
             # for MSE
-            y_pred = self._predict_inner(x_batched,params)
-            y_pred_grads = self._estimate_grad(x_batched,params)
-            grads = 2*(y_pred-y_batched) * y_pred_grads
+            y_pred = self._predict_inner(x_batched, params)
+            y_pred_grads = self._estimate_grad(x_batched, params)
+            grads = 2 * (y_pred - y_batched) * y_pred_grads
             grads = np.mean(grads, axis=0)
         else:
             embed_circuits = self._embed_x_circuit(x_batched)
@@ -220,16 +277,18 @@ class QNNRegressor:
         # print(f"{grads=}")
 
         return grads
-    
-    # * 未使用 
-    def _embed_x_circuit(self,x_scaled: NDArray[np.float_]) -> List[ParametricCircuitQuantumState]:
+
+    # * 未使用
+    def _embed_x_circuit(
+        self, x_scaled: NDArray[np.float_]
+    ) -> List[ParametricCircuitQuantumState]:
         circuits = []
         for x in x_scaled:
             circuit = UnboundParametricQuantumCircuit(self.n_qubits)
             for i in range(self.n_qubits):
                 circuit.add_RY_gate(i, np.arcsin(x) * 2)
                 circuit.add_RZ_gate(i, np.arccos(x * x) * 2)
-            
+
             # bind_circuit = self.ansatz.bind_parameters(params)
             circuit = circuit.combine(self.ansatz)
             parametric_circuit = ParametricCircuitQuantumState(self.n_qubits, circuit)
@@ -237,21 +296,23 @@ class QNNRegressor:
 
         return circuits
 
-    def _batch(self,data:NDArray[np.float_],batch_size:int):
-        for i in range(0,len(data),batch_size):
-            if i+batch_size > len(data):
+    def _batch(self, data: NDArray[np.float_], batch_size: int):
+        for i in range(0, len(data), batch_size):
+            if i + batch_size > len(data):
                 yield data[i:]
             else:
-                yield data[i:i+batch_size]
+                yield data[i : i + batch_size]
 
-    def _estimate_grad(self, x_scaled: NDArray[np.float_], params: Sequence[float]) -> NDArray[np.float_]:
+    def _estimate_grad(
+        self, x_scaled: NDArray[np.float_], params: Sequence[float]
+    ) -> NDArray[np.float_]:
         """
         Estimate the gradient of the cost function.
 
         Parameters:
             x_scaled: Input data whose shape is (batch_size, n_features).
             params: Parameters for the quantum circuit.
-        
+
         Returns:
             grads: Gradients of the cost function.
         """
@@ -259,39 +320,44 @@ class QNNRegressor:
         n_all_params = self.ansatz.n_parameters
         n_params = self.ansatz.parameter_count
         for x in x_scaled:
-            circuit_params = self.ansatz.generate_bound_params(x,params)
+            circuit_params = self.ansatz.generate_bound_params(x, params)
             circuit = ParametricCircuitQuantumState(self.n_qubits, self.ansatz.circuit)
-            estimate = self.gradient_estimator(self.operator,circuit,circuit_params)
+            estimate = self.gradient_estimator(self.operator, circuit, circuit_params)
             # input用のparamsを取り除く
-            grad = estimate.values[n_all_params-n_params:]
+            grad = estimate.values[n_all_params - n_params :]
             grads.append(grad)
         return np.asarray(grads)
 
-    def _predict_inner(self, x_scaled: NDArray[np.float_], params: Sequence[float]) -> NDArray[np.float_]:
+    def _predict_inner(
+        self, x_scaled: NDArray[np.float_], params: Sequence[float]
+    ) -> NDArray[np.float_]:
         """
         Predict inner function.
 
         Parameters:
             x_scaled: Input data whose shape is (batch_size, n_features).
             params: Parameters for the quantum circuit.
-        
+
         Returns:
             res: Predicted outcome.
         """
         res = []
         if isinstance(self.ansatz, LearningCircuit):
             for x in x_scaled:
-                circuit_params = self.ansatz.generate_bound_params(x,params)
-                c = self.ansatz.bind_input_and_parameters(x,params)
-                parametric_circuit = ParametricCircuitQuantumState(self.n_qubits, self.ansatz.circuit)
+                circuit_params = self.ansatz.generate_bound_params(x, params)
+                c = self.ansatz.bind_input_and_parameters(x, params)
+                parametric_circuit = ParametricCircuitQuantumState(
+                    self.n_qubits, self.ansatz.circuit
+                )
                 # if res == []:
                 #     for gate in c.gates:
                 #         print(f"name:{gate.name} params:{gate.params}")
-                estimate = self.estimator(self.operator,parametric_circuit,circuit_params)
+                estimate = self.estimator(
+                    self.operator, parametric_circuit, circuit_params
+                )
                 # ? これはout_dim == 1のみの応急処理
                 res.append([estimate.value.real])
             # print(f"{res=}")
-
 
         else:
             for x in x_scaled:
@@ -299,14 +365,14 @@ class QNNRegressor:
                 for i in range(self.n_qubits):
                     circuit.add_RY_gate(i, np.arcsin(x) * 2)
                     circuit.add_RZ_gate(i, np.arccos(x * x) * 2)
-                
+
                 # bind_circuit = self.ansatz.bind_parameters(params)
                 circuit = circuit.combine(self.ansatz)
-                parametric_circuit = ParametricCircuitQuantumState(self.n_qubits, circuit)
-                estimate = self.estimator(self.operator,parametric_circuit,params)
+                parametric_circuit = ParametricCircuitQuantumState(
+                    self.n_qubits, circuit
+                )
+                estimate = self.estimator(self.operator, parametric_circuit, params)
                 res.append(estimate.value.real)
-        
+
         # print(f"{res=}")
         return np.asarray(res)
-            
-
--- scikit_quri/test_reg.py
+++ scikit_quri/test_reg.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import sys
+
 sys.path.append(str(Path(__file__).resolve().parent.parent))
 
 # import better_exceptions
@@ -7,7 +8,10 @@ from scikit_quri.qnn.regressor import QNNRegressor
 import numpy as np
 from typing import Optional
 from quri_parts.circuit.utils.circuit_drawer import draw_circuit
-from quri_parts.circuit import UnboundParametricQuantumCircuit,LinearMappedUnboundParametricQuantumCircuit
+from quri_parts.circuit import (
+    UnboundParametricQuantumCircuit,
+    LinearMappedUnboundParametricQuantumCircuit,
+)
 from numpy.random import default_rng
 
 from qulacs import Observable
@@ -38,44 +42,53 @@ def create_farhi_neven_ansatz(
             circuit.add_ParametricRX_gate(zyu[i])
     return circuit
 
+
 def preprocess_x(x: np.ndarray, i: int) -> float:
     xa = x[i % len(x)]
-    clamped = min(1,max(-1,xa))
+    clamped = min(1, max(-1, xa))
     return clamped
 
-def create_farhi_neven_ansatz(n_qubit:int,c_depth:int):
+
+def create_farhi_neven_ansatz(n_qubit: int, c_depth: int):
     linear_param_circuit = LinearMappedUnboundParametricQuantumCircuit(n_qubit)
-    theta,x = linear_param_circuit.add_parameters("theta","x")
+    theta, x = linear_param_circuit.add_parameters("theta", "x")
     for i in range(n_qubit):
-        linear_param_circuit.add_ParametricRY_gate(i,{theta:1.})
-        linear_param_circuit.add_ParametricRZ_gate(i,lambda x,i=i: np.arccos(preprocess_x(x,i) * preprocess_x(x,i)))
+        linear_param_circuit.add_ParametricRY_gate(i, {theta: 1.0})
+        linear_param_circuit.add_ParametricRZ_gate(
+            i, lambda x, i=i: np.arccos(preprocess_x(x, i) * preprocess_x(x, i))
+        )
     zyu = list(range(n_qubit))
     rng = default_rng(0)
     for _ in range(c_depth):
         rng.shuffle(zyu)
         for i in range(0, n_qubit - 1, 2):
             pass
+
+
 def _create_farhi_neven_ansatz(
     n_qubit: int, c_depth: int, seed: Optional[int] = 0
 ) -> LearningCircuit:
     circuit = LearningCircuit(n_qubit)
     rng = default_rng(seed)
     for i in range(n_qubit):
-        circuit.add_input_RY_gate(i,lambda x,i=i: np.arcsin(preprocess_x(x,i)))
-        circuit.add_input_RZ_gate(i,lambda x,i=i: np.arccos(preprocess_x(x,i) * preprocess_x(x,i)))
-    
+        circuit.add_input_RY_gate(i, lambda x, i=i: np.arcsin(preprocess_x(x, i)))
+        circuit.add_input_RZ_gate(
+            i, lambda x, i=i: np.arccos(preprocess_x(x, i) * preprocess_x(x, i))
+        )
+
     zyu = list(range(n_qubit))
 
     for _ in range(c_depth):
         rng.shuffle(zyu)
         for i in range(0, n_qubit - 1, 2):
-            circuit.circuit.add_CNOT_gate(zyu[i+1],zyu[i])
+            circuit.circuit.add_CNOT_gate(zyu[i + 1], zyu[i])
             circuit.add_parametric_RX_gate(zyu[i])
             circuit.add_parametric_RY_gate(zyu[i])
-            circuit.circuit.add_CNOT_gate(zyu[i+1],zyu[i])
+            circuit.circuit.add_CNOT_gate(zyu[i + 1], zyu[i])
             circuit.add_parametric_RY_gate(zyu[i])
             circuit.add_parametric_RX_gate(zyu[i])
     return circuit
+
 
 def generate_noisy_sine(x_min, x_max, num_x):
     rng = default_rng(0)
@@ -86,53 +99,58 @@ def generate_noisy_sine(x_min, x_max, num_x):
     # return np.array(x_train), np.array(y_train)
     return np.array(x_train).flatten(), np.array(y_train)
 
+
 if __name__ == "__main__":
     from scikit_quri.circuit import LearningCircuit
     from quri_parts.circuit.utils.circuit_drawer import draw_circuit
     import matplotlib.pyplot as plt
+
     n_qubits = 2
     n_outputs = 1
-    parametric_circuit = _create_farhi_neven_ansatz(n_qubits,2,1)
+    parametric_circuit = _create_farhi_neven_ansatz(n_qubits, 2, 1)
     print(f"{parametric_circuit.circuit.gates_and_params=}")
     # parametric_circuit = LearningCircuit(n_qubits)
-    
+
     # parametric_circuit.bind_parameters()
     op = Operator()
     for i in range(n_outputs):
-        op.add_term(pauli_label(f"Z {i}"),1.0)
+        op.add_term(pauli_label(f"Z {i}"), 1.0)
+    # op.add_term(pauli_label("Z0"), 2.0)
+
     draw_circuit(parametric_circuit.circuit)
     estimator = create_qulacs_vector_parametric_estimator()
     concurrent_estimator = create_qulacs_vector_concurrent_parametric_estimator()
     gradient_estimator = create_parameter_shift_gradient_estimator(concurrent_estimator)
     adam = Adam()
     # Create Instance
-    qnn = QNNRegressor(n_qubits,parametric_circuit,estimator,gradient_estimator,adam,op)
-    x_train,y_train = generate_noisy_sine(-1.,1.,80)
-    x_test,y_test = generate_noisy_sine(-1.,1.,80)
+    qnn = QNNRegressor(
+        n_qubits, parametric_circuit, estimator, gradient_estimator, adam, op
+    )
+    x_train, y_train = generate_noisy_sine(-1.0, 1.0, 80)
+    x_test, y_test = generate_noisy_sine(-1.0, 1.0, 80)
     print(f"{x_train=}")
-    qnn.fit(x_train,y_train,len(x_train))
+    qnn.fit(x_train, y_train, len(x_train))
     y_pred = qnn.predict(x_test)
     print(f"{y_test.reshape(-1,1)=}")
     print(f"{y_pred=}")
 
     y_pred = y_pred.flatten()
-    plt.figure(figsize=(5,5))
+    plt.figure(figsize=(5, 5))
 
     # plt.plot(x_test,y_test,marker="o",color="orange",label="Test")
-    plt.plot(
-        np.sort(x_test),
-        y_test[np.argsort(x_test.flatten())],label="Test")
-    
-    x_true = np.linspace(-1,1,100)
+    plt.plot(np.sort(x_test), y_test[np.argsort(x_test.flatten())], label="Test")
+
+    x_true = np.linspace(-1, 1, 100)
     y_true = np.sin(np.pi * x_true)
-    plt.plot(x_true,y_true,label="True")
+    plt.plot(x_true, y_true, label="True")
     plt.plot(
         np.sort(x_test.flatten()),
         y_pred[np.argsort(x_test.flatten())],
-        label="Prediction")
+        label="Prediction",
+    )
     plt.legend()
-    
-    cost = np.mean((y_test- y_pred) ** 2)
+
+    cost = np.mean((y_test - y_pred) ** 2)
     print(f"{cost=}")
     # plt.show()
     plt.savefig("quri-parts.png")
