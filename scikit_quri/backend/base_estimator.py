1e849adccb9f3fc2aeaa809fd88bd6aba99883a5
Kazuma Tago <kazumatago@yahoo.co.jp>
2025-09-24 03:23:45 +0900

    fix: format実行

--- scikit_quri/backend/base_estimator.py
+++ scikit_quri/backend/base_estimator.py
@@ -8,12 +8,16 @@ from quri_parts.core.estimator import (
 )
 from quri_parts.core.estimator import Estimate
 
+
 class BaseEstimator(metaclass=ABCMeta):
     """Estimatorを実行する際の基底クラス
     estimateメソッドに対するinterfaceを定義
     """
+
     @abstractmethod
-    def estimate(self, operators: Sequence[Estimatable], states: Sequence[QulacsStateT]) -> Iterable[Estimate[complex]]:
+    def estimate(
+        self, operators: Sequence[Estimatable], states: Sequence[QulacsStateT]
+    ) -> Iterable[Estimate[complex]]:
         """
         operatorsとstatesの組み合わせに対して期待値を計算する
         operatorsまたはstatesのどちらかが1つの場合、もう一方の数に合わせて繰り返す
@@ -23,7 +27,7 @@ class BaseEstimator(metaclass=ABCMeta):
         Args:
             operators: 期待値を計算する演算子のリスト
             states: 期待値を計算する状態のリスト
-        
+
         Returns:
             operatorsとstatesの組み合わせに対する期待値のリスト
         """
